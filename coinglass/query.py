e11621c98434e76938725a6207c765d862f1ec8d
AmbitionCX <chenxuanamazing@gmail.com>
2023-03-17 23:51:36 +0800

    add data

--- coinglass/query.py
+++ coinglass/query.py
@@ -1,6 +1,9 @@
 import os
+import io
 from os.path import join, dirname
 import requests
+import pandas as pd
+from datetime import datetime
 from dotenv import load_dotenv
 
 dotenv_path = join(dirname(__file__), '.env')
@@ -12,12 +15,27 @@ base_url= "https://open-api.coinglass.com"
 api_endpoint = "/public/v2/open_interest_history"
 
 symbol = "BTC" 
-currency = "USDT"
-time_type = "h12" # m1, m5, m15, h1, h4, h12, all
+currency = "USD"
+time_type = "all" # m1, m5, m15, h1, h4, h12, all
 
 url = base_url + api_endpoint + "?symbol=" + symbol + "&currency=" + currency + "&time_type=" + time_type
 
 headers = {"accept": "application/json", "coinglassSecret": api_key}
-response = requests.get(url, headers=headers)
+response = requests.get(url, headers=headers).json()
 
-print(response.json())
+rawData = response["data"]
+
+local_dict = {}
+for key, value in rawData.items():
+    if type(value) is dict:
+        for k, v in value.items():
+            if k == "Binance":
+                local_dict[k] = v
+    else:
+        local_dict[key] = value
+
+pdframe = pd.DataFrame(local_dict)
+pdframe["dateList"]=[datetime.fromtimestamp(x).strftime("%x %X") for x in pdframe["dateList"].values/1000]
+pdframe.to_csv("BTC_USDT_Binance.csv", sep=',')
+
+# print(pdframe)
